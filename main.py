dcb239013dc9931a4c9c6a80dcc0299de79b7158
YanisBelkacemi <108769998+YanisBelkacemi@users.noreply.github.com>
2026-03-07 16:34:15 +0100

    Adding the post input route

--- main.py
+++ main.py
@@ -3,11 +3,11 @@ import requests
 from request import prompting
 
 input = input("enter the prompt")
-ollama_url = 'http://localhost:11434/api/generate'
-
 app = FastAPI()
 
-@app.get('/')
+@app.get('/output')
 def output():
     return {'response' :prompting(input)}
-
+@app.post('/input')
+def input():
+    pass
