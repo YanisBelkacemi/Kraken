f9f7b02ce4ceb1e4ef08069a895caf86a6b2eb5d
Yanis Belkacemi <108769998+YanisBelkacemi@users.noreply.github.com>
2026-03-08 12:39:04 +0100

    Add payload example in prompting function
    Added an example of the payload format for clarity.

--- request.py
+++ request.py
@@ -3,6 +3,7 @@ import requests
 def prompting(prompt):
     url = 'http://localhost:11434/api/generate'
     payload = prompt
+#exmaple of the payload :
     #{'model' : 'tinyllama' , 'prompt' : prompt, 'stream' : False}
 
     resp = requests.post(url , json=payload, stream=False)
